06b75c61d73e2a7cfd069611b9c8ffc00c8a4685
Veer Kataria <underc0veer2@gmail.com>
2024-05-22 08:12:53 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let a = 3;
+let b = 5;
+let c;
+
+console.log("a + b = " + (a + b) + "\n" +
+      "a - b = " + (a - b) + "\n" +
+      "a * b = " + (a * b) + "\n" +
+      "a / b = " + (a / b) + "\n" +
+      "a % b = " + (a % b) + "\n" +
+      "a += b = " + (a += b) + "\n" +
+      "a -= b = " + (a -= b) + "\n" +
+      "a = b = " + (a= b) + "\n" +
+      "a /= b = " + (a /= b) + "\n" +
+      "a %= b = " + (a %= b) + "\n" +
+      "a == b = " + (a == b) + "\n" +
+      "a != b = " + (a != b) + "\n" +
+      "a > b = " + (a > b) + "\n" +
+      "a < b = " + (a < b) + "\n" +
+      "!a && !c = " + (!a && !c) + "\n" +
+      "!a || !c = " + (!a ||!c));
+
+let first_name = "Veer";
+let last_name = "Kataria";
+let email = "kata0053@algonquinlive.com";
+let output = `My name is ${first_name} ${last_name}. You can contact me at ${email}`;
+
+console.log(output);
